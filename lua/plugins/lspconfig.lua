0630395a2e43b023230cf934e8bc62f313256d90
cachedpotato <chiwonchung98@gmail.com>
2025-02-05 23:58:41 +0900

    rustaceanvim & some changes to lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,10 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
-    event = {"BufReadPre", "BufNewFile"},
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         { "antosha417/nvim-lsp-file-operations", config = true },
-        { "folke/neodev.nvim", opts = {} },
+        { "folke/neodev.nvim",                   opts = {} },
     },
     config = function()
         local lspconfig = require("lspconfig")
@@ -25,11 +25,14 @@ return {
         })
         mason_lspconfig.setup_handlers({
             --default setup function for installed servers
-            function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
+            --function(server_name)
+            --    lspconfig[server_name].setup({
+            --        capabilities = capabilities,
+            --    })
+            --end,
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
         })
     end,
 }
