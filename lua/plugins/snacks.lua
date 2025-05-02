79d1b05f9dab4ab631a3c71fdb24792317027ea7
cachedpotato <chiwonchung98@gmail.com>
2025-05-02 13:53:37 +0900

    update in ages

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -51,14 +51,14 @@ return {
                     total = 500, -- maximum duration
                 },
             },
-            scope = {
-                enabled = true, -- enable highlighting the current scope
-                priority = 200,
-                char = "│",
-                underline = false,    -- underline the start of the scope
-                only_current = false, -- only show scope in the current window
-                hl = "SnacksIndentScope", ---@type string|string[] hl group for scopes
-            },
+            -- scope = {
+            --     enabled = true, -- enable highlighting the current scope
+            --     priority = 200,
+            --     char = "│",
+            --     underline = false,    -- underline the start of the scope
+            --     only_current = false, -- only show scope in the current window
+            --     hl = "SnacksIndentScope", ---@type string|string[] hl group for scopes
+            -- },
         },
 
         -- Git utils
